b0887e9a8e78b5ab85c40bdb69f647d3eabb94c9
GuyvandermeulenDeltion <guy_meulen@live.nl>
2019-01-26 18:40:49 +0100

    Titlescreen Changes

---
+++ Gamejam_Bois/Assets/Individual/Koen/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    private static float loadTime = 3;
+
+    public void LoadMain() {
+        StartCoroutine(StartGameTimer());
+    }
+
+    private IEnumerator StartGameTimer() {
+        yield return new WaitForSeconds(loadTime);
+        SceneManager.LoadScene(1);
+    }
+}
