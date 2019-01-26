1660798d571875944eabddba5736036a888a4c1a
sebastiaangroenink <sebastiaangroenink@gmail.com>
2019-01-26 20:10:06 +0100

    Teleportation Added

---
+++ Gamejam_Bois/Assets/Individual/Raphael/Scripts/FadeToBlack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeToBlack : MonoBehaviour {
+
+    public static FadeToBlack instance;
+
+    public Image screen;
+    public float fadeSpeed;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator FadeTo()
+    {
+        while (screen.color.a < 1)
+        {
+            screen.color = new Color(screen.color.r, screen.color.g, screen.color.b, screen.color.a + Time.deltaTime * fadeSpeed);
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeFrom()
+    {
+        while (screen.color.a > 0)
+        {
+            screen.color = new Color(screen.color.r, screen.color.g, screen.color.b, screen.color.a - Time.deltaTime * fadeSpeed);
+            yield return null;
+        }
+    }
+    
+}
--- Gamejam_Bois/Assets/Individual/Raphael/Scripts/TeleportationField.cs
+++ Gamejam_Bois/Assets/Individual/Raphael/Scripts/TeleportationField.cs
@@ -8,7 +8,19 @@ public class TeleportationField : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.transform.position = teleportTo.transform.position;
-        collision.transform.rotation = teleportTo.transform.rotation;
+        StartCoroutine(Teleport(collision.transform));
+    }
+
+    public IEnumerator Teleport(Transform player)
+    {
+        float seconds = FadeToBlack.instance.fadeSpeed;
+
+        StartCoroutine(FadeToBlack.instance.FadeTo());
+        yield return new WaitForSeconds(seconds);
+
+        player.position = teleportTo.transform.position;
+        player.rotation = teleportTo.transform.rotation;
+
+        StartCoroutine(FadeToBlack.instance.FadeFrom());
     }
 }
