14f85c36094cc911a14e3b93aa680281c8d794cd
GuyvandermeulenDeltion <guy_meulen@live.nl>
2019-01-26 20:32:27 +0100

    Added knife sound

---
+++ Gamejam_Bois/Assets/Individual/Koen/Scripts/PlaySound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour {
+
+    [Header("Sound:")]
+    public string clipName;
+
+    public void PlayAudio() {
+        GetComponent<AudioSource>().PlayOneShot((AudioClip)Resources.Load(clipName));
+    }
+}
