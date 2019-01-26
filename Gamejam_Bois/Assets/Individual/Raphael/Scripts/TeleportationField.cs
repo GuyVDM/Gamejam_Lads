de6a1aa750a803943873b44cc1b904b546a8c4f2
sebastiaangroenink <sebastiaangroenink@gmail.com>
2019-01-26 18:06:31 +0100

    Teleportation Started

---
+++ Gamejam_Bois/Assets/Individual/Raphael/Scripts/TeleportationField.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationField : MonoBehaviour {
+
+    public Transform teleportTo;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.position = teleportTo.transform.position;
+        collision.transform.rotation = teleportTo.transform.rotation;
+    }
+}
