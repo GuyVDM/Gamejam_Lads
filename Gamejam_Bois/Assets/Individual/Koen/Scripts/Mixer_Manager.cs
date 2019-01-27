c2793903fc5c271d93afc8aca6c3d02d819a4c1e
GuyvandermeulenDeltion <guy_meulen@live.nl>
2019-01-27 11:37:07 +0100

    Bear

---
+++ Gamejam_Bois/Assets/Individual/Koen/Scripts/Mixer_Manager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Mixer_Manager : MonoBehaviour {
+
+    [SerializeField]private AudioMixer masterMixer;
+
+    public void SetSound(float soundLevel) {
+        masterMixer.SetFloat("musicVol", soundLevel);
+    }
+}
+
